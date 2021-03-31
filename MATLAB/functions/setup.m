e30eefbb6b4981faea0d723db1d9ea3b5fc7f09d
mitchjbrown11 <mitch.j.brown@outlook.com>
2021-03-31 14:30:49 +1100

    Update setup.m
    setup for SLAM example

--- MATLAB/functions/setup.m
+++ MATLAB/functions/setup.m
@@ -10,27 +10,38 @@ function [opt] = setup(opt)
 %% References:
     % 
 
+addpath('data/datasets')
 %% run code
+opt.active = true; % Must be true for SLAM solution to run. will go false when simulation has ended.
+
 % Get states, input & measurement sizes
 opt.nx = size(vehicleModel(opt),1); % number of states
 for u = 1:10
     try
         vehicleModel(opt,ones(opt.nx,1),ones(u,1));
         opt.nu = u; % number of inputs
-        break     
+        break;
     catch
-        %
+        % try again
     end
 end
 
-opt.ny = 2;   % number or Landmark states
+opt.ny = 2;   % number or Landmark states (NEED TO MAKE DYNAMIC)
+opt.Delta = 0.1;
 
 % Initialse Variables
 opt.nLandmarks = 0;
 
+% setup track, cones for sim
+opt = generateLandmarks(opt);
+c = load(opt.controllerInput);
+if size(c.u,1) > size(c.u,2)
+    c.u = c.u';
+end
+
 % Setup Ground truth: Measurements, inputs and states
 opt.gt.z = []; % measured landmarks is a 2x? vector
-opt.gt.u = zeros(opt.nu,1);
+opt.gt.u = c.u;
 opt.gt.x = zeros(opt.nx+opt.ny*opt.nLandmarks,1);
 
 % Setup observed matrices: as percieved from sensors
@@ -46,9 +57,17 @@ opt.Pf = zeros(opt.nx + opt.ny*opt.nLandmarks);
 opt.xp = zeros(opt.nx + opt.ny*opt.nLandmarks,1);
 for i = 1: length(opt.gt.initPose)
     opt.xp(i) = opt.gt.initPose(i); % assume x, y, psi as states 1:3
+    opt.gt.x(i) = opt.gt.initPose(i);
 end
-opt.Pp = 100.*eye(opt.nx + opt.ny*opt.nLandmarks);
+opt.Pp = 0.1.*eye(opt.nx + opt.ny*opt.nLandmarks);
 
+% parameters for range/bearing model
+opt.par_rb.maxRange = 40;
+opt.par_rb.angle = 270;
+
+% Noise amplitude
+opt.par_rb.noise_range = 0.1;
+opt.par_rb.noise_angle = 0.001;
 
 %% end of function
 end
